bd9a9ba980620e91c04c5dc4909c06673dd0c649
shubham <ss691438@gmail.com>
2024-05-11 13:26:04 +0530

    minor fix

--- frontend/src/components/DateFilter.jsx
+++ frontend/src/components/DateFilter.jsx
@@ -52,7 +52,7 @@ const DateFilter = ({taskList, setTaskList,selectedDate,handleFilterDateChange})
                                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                             </svg>
                         </div>
-                        <input type="text" className="border text-sm rounded-lg block w-full ps-10 p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Select Date" value={selectedDate} onFocus={() => setShow(true)} readOnly />
+                        <input type="text" className="border text-sm rounded-lg block w-full ps-10 p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="filter by date" value={selectedDate} onFocus={() => setShow(true)} readOnly />
                     </div>
                 </Datepicker>
 		    </div>
